b45d2836bf609b441e0957b305e650a247b74f46
A.G <aayushg@mit.edu>
2021-07-13 16:47:39 -0700

    cp not mv

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ ln -sf `pwd`/.tmux.remote.conf ~/.tmux.remote.conf
 ln -sf `pwd`/.inputrc ~/.inputrc
 ln -sf `pwd`/.config/nvim/ ~/.config/nvim
 
-sudo mv `pwd`/speak_date.sh /usr/local/bin/speak.sh
+sudo cp `pwd`/speak_date.sh /usr/local/bin/speak.sh
 sudo chmod 777 /usr/local/bin/speak.sh
-sudo mv `pwd`/speak_date.sh /usr/local/bin/speak_date.sh
+sudo cp `pwd`/speak_date.sh /usr/local/bin/speak_date.sh
 sudo chmod 777 /usr/local/bin/speak_date.sh
 
 # Idempotent append cron job
