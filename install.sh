c6b1881f48cea5f665a34de5835080c736d368f6
A.G <aayushg@mit.edu>
2021-07-13 16:45:02 -0700

    Idempotent add speak on hour cron job

--- install.sh
+++ install.sh
@@ -8,3 +8,8 @@ sudo mv `pwd`/speak_date.sh /usr/local/bin/speak.sh
 sudo chmod 777 /usr/local/bin/speak.sh
 sudo mv `pwd`/speak_date.sh /usr/local/bin/speak_date.sh
 sudo chmod 777 /usr/local/bin/speak_date.sh
+
+# Idempotent append cron job
+grep -Fxe '0 * * * * sh speak_date.sh' /etc/crontabs/root || {
+  sudo tee -a /etc/crontabs/root <<<"0 * * * * sh speak_date.sh"
+}
