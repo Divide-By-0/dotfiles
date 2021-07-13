1b05f26a49794ab590acaaf842a5031d870ff71c
A.G <aayushg@mit.edu>
2021-07-13 16:39:52 -0700

    Move speak and speak date to bin
    Need to add crontab

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@ ln -sf `pwd`/.tmux.conf ~/.tmux.conf
 ln -sf `pwd`/.tmux.remote.conf ~/.tmux.remote.conf
 ln -sf `pwd`/.inputrc ~/.inputrc
 ln -sf `pwd`/.config/nvim/ ~/.config/nvim
+
+sudo mv `pwd`/speak_date.sh /usr/local/bin/speak.sh
+sudo chmod 777 /usr/local/bin/speak.sh
+sudo mv `pwd`/speak_date.sh /usr/local/bin/speak_date.sh
+sudo chmod 777 /usr/local/bin/speak_date.sh
