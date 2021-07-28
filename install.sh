c01c054a0c3a1bb685d65848df10368bbf2e6fca
A.G <aayushg@mit.edu>
2021-07-27 22:17:15 -0700

    date to time

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ ln -sf `pwd`/.config/nvim/ ~/.config/nvim
 
 sudo cp `pwd`/speak_date.sh /usr/local/bin/speak.sh
 sudo chmod 777 /usr/local/bin/speak.sh
-sudo cp `pwd`/speak_date.sh /usr/local/bin/speak_date.sh
-sudo chmod 777 /usr/local/bin/speak_date.sh
+sudo cp `pwd`/speak_time.sh /usr/local/bin/speak_time.sh
+sudo chmod 777 /usr/local/bin/speak_time.sh
 
 # Idempotent bash aliases
 if [ ! -f ~/.bash_aliases ]
